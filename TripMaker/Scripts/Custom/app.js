e99f80bac3cc82c1a0e54272f65ef5262e59767f
Sumanth Nag <sumanthnag@yahoo.com>
2015-06-18 20:02:30 -0500

    UserTrip creation added and styles applied

--- TripMaker/Scripts/Custom/app.js
+++ TripMaker/Scripts/Custom/app.js
@@ -22,28 +22,42 @@
 
 
     app.controller('SubscriberController', ['$scope', '$http', 'tripService', function ($scope, $http, tripService) {
-        $scope.subscriber = { "userid": "" };
+        $scope.subscriber = { "userid": "" , 'name': ''};
         $scope.subscribername = '';
         $scope.tripname = '';
-        $scope.trip = { "name": "" }
-        $scope.usertrip = {
-            "userId": "dfae1558-a6d8-4bf5-9e51-23bbe0a7e2d2",
-            "tripId": "3492b76f-fecc-4f89-9763-9dfdd864ae27"
-        };
-        $scope.registeredSubscribers = ['Sub1', 'Sub12', 'Sub51'];
-            //tripService.getSubsribers();
-        $scope.allTrips = ['Tr1', 'Tr2', 'Tr3']; // tripService.getTrips();
-        $scope.message = '';
-        var OnSubscriberCreation = function (response) {           
-            tripService.saveSubsriber(response.data.links[0].name);
-            $scope.message = response.data;
-            
+        $scope.trip = { 'tripid': '', 'name': '' }
+        $scope.successmessage = '';
+
+        $scope.extractUIDFromURL = function (url) {
+            splitArray = url.split('/');
+            return splitArray[splitArray.length - 1]
         };
 
-        var OnSubscriberCreationT = function (response) {
-            tripService.saveTrips(response.data.links[0].name);
-            $scope.message = response.data;
+        $scope.usertrip = {
+            "userId": "",
+            "tripId": ""
         };
+        $scope.registeredSubscribers = tripService.getSubsribers();
+        $scope.allTrips = tripService.getTrips();
+        $scope.message = '';
+        var OnSubscriberCreation = function (response) {           
+            var subxkribr = { 'subscriberid': $scope.extractUIDFromURL(response.data.links[0].href), 'name': response.data.links[0].name }
+            tripService.saveSubsriber(subxkribr);
+            $scope.successmessage = 'Subscriber created';
+        };
+
+        var OnTripCreation = function (response) {
+            var tripp = { 'tripid': $scope.extractUIDFromURL(response.data.links[0].href), 'name': response.data.links[0].name }
+            tripService.saveTrips(tripp);
+            $scope.successmessage = 'Trip created';
+        };
+
+        var OnUserTripCreation = function (response) {
+            var userTripp = { 'usertripid': $scope.extractUIDFromURL(response.data.links[0].href), 'name': response.data.links[0].name }
+            console.log(userTripp);
+            $scope.successmessage = 'Trip was associated with subscriber : ' + response.data.links[0].href + ', ' + response.data.links[0].name ;
+        };
+
 
         var OnError = function (response) {
             $scope.message = response.data;           
@@ -56,18 +70,16 @@
         };
 
         $scope.CreateT = function () {
-            $http.post("http://api.doactively.com/v1/Trip", $scope.trip).then(OnSubscriberCreationT, OnError);
+            $http.post("http://api.doactively.com/v1/Trip", $scope.trip).then(OnTripCreation, OnError);
             console.log($scope.trip.name);
         };
 
         $scope.CreateUT = function () {
-            $http.post("http://api.doactively.com/v1/UserTrip", $scope.usertrip).then(OnSubscriberCreation, OnError);
+            $scope.usertrip.userId = $scope.subscribername.subscriberid;
+            $scope.usertrip.tripid = $scope.tripname.tripid;
+            $http.post("http://api.doactively.com/v1/UserTrip", $scope.usertrip).then(OnUserTripCreation, OnError);
            
         };
-
-
-
-
 
     }]);
 }()
